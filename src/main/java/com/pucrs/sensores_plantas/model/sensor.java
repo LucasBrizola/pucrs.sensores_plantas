1ffc80cf3a06b64ec7f089eaf32f4eeeb1d8e239
Java <Java@10.32.162.110>
2019-07-09 16:29:12 -0300

    #7

---
+++ src/main/java/com/pucrs/sensores_plantas/model/sensor.java
@@ -0,0 +1,21 @@
+package com.pucrs.sensores_plantas.model;
+
+public class sensor {
+	private int id;
+	private int umidade;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUmidade() {
+		return umidade;
+	}
+	public void setUmidade(int umidade) {
+		this.umidade = umidade;
+	}
+	
+	
+	
+}
