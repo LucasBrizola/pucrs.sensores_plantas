794adb22f34a180e09dc6729ab7db205c39dd911
lucastashan <lucastashan@gmail.com>
2019-07-09 15:15:12 -0300

    #5

---
+++ src/main/java/com/pucrs/sensores_plantas/SensoresPlantasApplication.java
@@ -0,0 +1,13 @@
+package com.pucrs.sensores_plantas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensoresPlantasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensoresPlantasApplication.class, args);
+	}
+
+}
